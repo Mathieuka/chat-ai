e78c31186a5eee7e52bc3cb3fd44b10920eb8122
Mathieu Kahlaoui <34446722+Mathieuka@users.noreply.github.com>
2025-10-04 17:03:54 +0200

    feat: add message input functionality and enhance ChatView layout

--- src/components/ChatView.vue
+++ src/components/ChatView.vue
@@ -17,17 +17,52 @@ defineProps<{
         <p>{{ message.timestamp.getTime() }}</p>
       </li>
     </ul>
+    <div class="message-input-container">
+      <input class="message-input" type="text" />
+      <button class="message-button">Send</button>
+    </div>
   </div>
 </template>
 
 <style scoped>
+.chat-container {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+}
+
 .messages {
   display: flex;
   flex-direction: column;
+  overflow-y: auto;
+  max-height: calc(100vh - 3rem); /* Adjust based on input area height */
+  padding: 0;
+  margin: 0;
+  list-style: none;
 }
 
 .message {
   display: flex;
   justify-content: space-between;
+  border-bottom: 1px solid #eee;
+  padding: 0.5rem;
+}
+
+.message-input-container {
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+}
+
+.message-input {
+  width: 80%;
+  height: 2rem;
+  border-top: 1px solid black;
+  background: white;
+}
+
+.message-button {
+  height: 2.3rem;
+  width: 20%;
 }
 </style>
